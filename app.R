422b762981b12fd5547cea5d879b47d78e658887
Yousuf Ali <yousuf.pharma@gmail.com>
2024-01-09 09:30:28 -0500

    update

--- app.R
+++ app.R
@@ -2,4 +2,4 @@ devtools::document()
 devtools::load_all('.')
 path <-  'C:/Users/Md.Ali/OneDrive - FDA/yousuf/10_DATA/Biocelerate_shared_data/data/biocelerate.db'
 ## path <-  'C:/Users/Md.Ali/OneDrive - FDA/yousuf/10_DATA/susan_db_cross_study/susan_test.db'
-SENDcross::send_cross_study_app(database_path = path)
+sendSummarizer::send_cross_study_app(database_path = path)
